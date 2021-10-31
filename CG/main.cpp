6a2238159607f24e7e8e9205a627861e526e5d5f
SleepinWei <zyw0241@outlook.com>
2021-10-31 21:58:57 +0800

    Init

---
+++ CG/main.cpp
@@ -0,0 +1,11 @@
+#define STB_IMAGE_IMPLEMENTATION
+#include<glad/glad.h>
+#include<iostream>
+#include<glfw/glfw3.h>
+#include<glm/glm.hpp>
+
+#include<stb/stb_image.h>
+
+int main() {
+	std::cout << "hello world" << '\n';
+}
