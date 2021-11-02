92c257b67024fc051ea6d1d51f0ea05c35ad8adb
SleepinWei <zyw0241@outlook.com>
2021-11-03 00:08:04 +0800

    add assimp

--- CG/Mesh.h
+++ CG/Mesh.h
@@ -2,7 +2,7 @@
 #define MESH_H
 #include<glm/glm.hpp>
 #include<glm/gtc/matrix_transform.hpp>
-#include<shader/Shader.cpp>
+#include<shader/Shader.h>
 #include<string>
 #include<vector>
 
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # CG-project
+Our course project of Computer Graphics 
+## Group Members 
+
+## Structure 
