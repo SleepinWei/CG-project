270eaedcb91ff6ab0573376bff6044e663dc081b
SleepinWei <zyw0241@outlook.com>
2021-11-06 00:38:30 +0800

    modify assimp

--- CG/main.cpp
+++ CG/main.cpp
@@ -1,10 +1,15 @@
-#define STB_IMAGE_IMPLEMENTATION
 #include<glad/glad.h>
 #include<iostream>
 #include<glfw/glfw3.h>
 #include<glm/glm.hpp>
 
-#include<stb/stb_image.h>
+//#include<stb/stb_image.h>
+#include"Mesh.h"
+#include"Model.h"
+#include"Camera.h"
+#define STB_IMAGE_IMPLEMENTATION
+#include"stb_image.h"
+
 
 int main() {
 	std::cout << "hello world" << '\n';
